a887acf877206eca9f39494039968597de5c18d0
pranjal verma <pranjalverma975@gmail.com>
2025-01-06 00:48:37 +0530

    first commit

---
+++ scripts/theme-toggle.js
@@ -0,0 +1,19 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeToggle.checked = true;
+} else {
+    document.body.classList.remove('dark-mode');
+    themeToggle.checked = false;
+}
+
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+    }
+});
